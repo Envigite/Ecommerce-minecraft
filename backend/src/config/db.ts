1058c28b64bc43229679b951080d59b630c68718
Envigite <benjacontrerasma@yahoo.com>
2025-12-10 11:58:07 -0300

    cambio db

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,13 +1,5 @@
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
-import fs from "fs";
 import { Pool } from "pg";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const caPath = join(__dirname, "../../config/us-east-2-bundle.pem");
-const isProduction = process.env.NODE_ENV === "production";
 
 export const pool = new Pool({
   host: process.env.DB_HOST,
@@ -15,9 +7,7 @@ export const pool = new Pool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: 5432,
-  ssl: isProduction ? {
-    ca: fs.readFileSync(caPath).toString(),
-  } : {
+  ssl: {
     rejectUnauthorized: false,
   },
 });
